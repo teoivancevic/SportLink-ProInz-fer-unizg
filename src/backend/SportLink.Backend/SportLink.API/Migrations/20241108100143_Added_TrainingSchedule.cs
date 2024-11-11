2b1f1de625cf49c67a9c783eb0020a8b4200fc53
rokojakasa <rocoyo03@gmail.com>
2024-11-11 23:03:04 +0100

    Migrations

---
+++ src/backend/SportLink.Backend/SportLink.API/Migrations/20241108100143_Added_TrainingSchedule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportLink.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_TrainingSchedule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Tournament",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "TrainingSchedule",
+                columns: table => new
+                {
+                    DayOfWeek = table.Column<int>(type: "integer", nullable: false),
+                    StartTime = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    TrainingGroupId = table.Column<int>(type: "integer", nullable: false),
+                    EndTime = table.Column<TimeSpan>(type: "interval", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrainingSchedule", x => new { x.TrainingGroupId, x.DayOfWeek, x.StartTime });
+                    table.ForeignKey(
+                        name: "FK_TrainingSchedule_TrainingGroup_TrainingGroupId",
+                        column: x => x.TrainingGroupId,
+                        principalTable: "TrainingGroup",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TrainingSchedule");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Tournament");
+        }
+    }
+}
