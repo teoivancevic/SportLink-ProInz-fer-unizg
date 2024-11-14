b766edd212c84b8d49798a6615e002a2da025c05
rokojakasa <rocoyo03@gmail.com>
2024-11-14 20:39:21 +0100

    Configured User.cs for external login

--- src/backend/SportLink.Backend/SportLink.API/Data/Entities/User.cs
+++ src/backend/SportLink.Backend/SportLink.API/Data/Entities/User.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SportLink.Core.Enums;
 
 namespace SportLink.API.Data.Entities;
 
@@ -12,13 +14,16 @@ public class User : BaseEntity
     public int RoleId { get; set; }
     
     
-    public byte[] PasswordHash { get; set; }
-    public byte[] PasswordSalt { get; set; }
+    public byte[]? PasswordHash { get; set; }
+    public byte[]? PasswordSalt { get; set; }
     
     public DateTime LastLoginAt { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public bool IsEmailVerified { get; set; }
+    
+    public ExternalUserSourceEnum? ExternalUserSource { get; set; }
+    public string? ExternalUserId { get; set; }
     
     public virtual Role Role { get; set; }
     public virtual ICollection<OTPCode> OTPCodes { get; set; }
@@ -28,6 +33,7 @@ public class User : BaseEntity
 
 public class UserConfigurationBuilder : IEntityTypeConfiguration<User>
 {
+    private readonly EnumToStringConverter<ExternalUserSourceEnum> _converter = new EnumToStringConverter<ExternalUserSourceEnum>();
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.ToTable(nameof(User));
@@ -43,9 +49,9 @@ public class UserConfigurationBuilder : IEntityTypeConfiguration<User>
             .IsUnique();
         
         builder.Property(x => x.PasswordHash)
-            .IsRequired();
+            .IsRequired(false);
         builder.Property(x => x.PasswordSalt)
-            .IsRequired();
+            .IsRequired(false);
         
         builder.Property(x => x.CreatedAt)
             .IsRequired();
@@ -56,6 +62,12 @@ public class UserConfigurationBuilder : IEntityTypeConfiguration<User>
         builder.Property(x => x.IsEmailVerified)
             .HasDefaultValue(false)
             .IsRequired();
+        
+        builder.Property(x => x.ExternalUserId)
+            .IsRequired(false);
+        builder.Property(x => x.ExternalUserSource)
+            .HasConversion(_converter)
+            .IsRequired(false);
         
         builder.HasOne(x => x.Role)
             .WithMany(r => r.Users)
