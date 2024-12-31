2e0d829d212adce6f0433377f7bc6841472d788d
Teo Ivančević <teo@tomsoft.hr>
2024-12-31 02:28:04 +0100

    fix dockerfile

--- src/frontend/sportlink-web-3/next.config.mjs
+++ src/frontend/sportlink-web-3/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'standalone',
+    experimental: {
+      outputFileTracingRoot: undefined 
+    }
+  }
+  
+  module.exports = nextConfig
