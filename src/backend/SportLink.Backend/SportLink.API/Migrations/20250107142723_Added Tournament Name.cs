226fffc566ad4a4a40d01e889904d00e97e28c87
rokojakasa <rocoyo03@gmail.com>
2025-01-07 15:27:45 +0100

    Fixed database context and added tournament name

---
+++ src/backend/SportLink.Backend/SportLink.API/Migrations/20250107142723_Added Tournament Name.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportLink.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTournamentName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Tournament",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Tournament");
+        }
+    }
+}
