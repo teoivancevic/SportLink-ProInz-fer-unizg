db02f64b21ff9b732f082c3a085dd4265946f228
rokojakasa <rocoyo03@gmail.com>
2025-01-13 17:57:50 +0100

    Integration tests project initialization

---
+++ src/backend/SportLink.Backend/SportLink.Api.IntegrationTests/SportLinkWebApplicationFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using SportLink.API.Data;
+
+namespace SportLink.Api.IntegrationTests;
+
+internal class SportLinkWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<DataContext>));
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseInMemoryDatabase("SportLink TestDb");
+            });
+        });
+                
+        
+    }
+}
