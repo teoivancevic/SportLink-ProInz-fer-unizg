3c8298420358e6b5a30b9b49d7209e0b3bc8e65c
cvita99 <lovro.cvitanovic99@gmail.com>
2025-01-24 19:29:14 +0100

    fixed working times in sport object, fixed types for SportObject

--- src/frontend/sportlink-web-3/types/sportObject.ts
+++ src/frontend/sportlink-web-3/types/sportObject.ts
@@ -1,21 +1,35 @@
-export interface SportsObject {
-    id: number,
-    name: string,
-    location: string, 
-    organizationName: string,
-    sportCourtDtos: SportCourtDto[]
-    organizationId: number
-}
+export interface WorkTime {
+    id?: number; 
+    dayOfWeek: number;
+    isWorking?: boolean; 
+    openFrom: string;
+    openTo: string;
+    sportsObjectId: number;
+  }
+  
+  export interface SportCourt {
+    id: number;
+    sportsObjectId: number;
+    availableCourts: number;
+    maxHourlyPrice: number;
+    sportName?: string;
+    sportId?: number;
+  }
+  
+  export interface SportObject {
+    id: number;
+    name: string;
+    description: string;
+    location: string;
+    organizationId?: number;
+    workTimes: WorkTime[];
+    sportCourts: SportCourt[];
+  }
 
-export interface SportCourtDto {
-    id: number,
-    availableCourts: number,
-    sportName: string,
-    minHourlyPrice: number,
-    maxHourlyPrice: number,
-    sportsObjectId: number
-}
+  export interface getSportObjectsResponse {
+    data: SportObject[];
+  }
 
-export interface searchSportsObjectsResponse {
-    data: SportsObject[]
-}
+
+  
+  
